5dabf787fd38442cc32802d2689bb50d169dd92b
Sean <powerwaffe@gmail.com>
2017-02-02 15:29:03 -0500

    Added images and changed layout

--- app/src/main/java/edu/dtcc/sean/microwave/MainActivity.java
+++ app/src/main/java/edu/dtcc/sean/microwave/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.animation.Interpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
